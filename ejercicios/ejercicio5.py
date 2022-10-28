a2a6f163f48a3d47d44ed4d9c9d6db38b79debe4
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-28 16:42:27 +0200

    .

---
+++ ejercicios/ejercicio5.py
@@ -0,0 +1,19 @@
+import hashlib
+
+class Encriptacion():
+    def __init__(self, mensaje):
+        self.mensaje = mensaje.split(' ')
+        self.mensaje_encriptado = []
+    def encriptar(self):
+        for palabra in self.mensaje:
+            self.mensaje_encriptado.append(hashlib.sha256(str(palabra).encode('utf-8')).hexdigest())
+        with open('mensaje_encriptado.txt', 'w') as archivo:
+            archivo.write(' '.join(self.mensaje_encriptado))
+        return self.mensaje_encriptado
+
+def ejercicio5():
+    mensaje = input('Ingrese el mensaje a encriptar: ')
+    encriptacion = Encriptacion(mensaje)
+    print(encriptacion.encriptar())
+
+ejercicio5()
