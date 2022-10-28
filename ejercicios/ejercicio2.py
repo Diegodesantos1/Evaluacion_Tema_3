2afacef82ed5925e8890414b4853960cebb03b4c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-28 15:49:15 +0200

    ya funciona el determinante

--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -1,10 +1,13 @@
 
 def sarrus(matriz):
     if len(matriz) == 3:
-        solucion =((matriz[0][0] * matriz[1][1] * matriz[2][2]) + (matriz[0][1] * matriz[1][2] * matriz[2][0]) + (matriz[0][2] * matriz[1][0] * matriz[2][1]) - (matriz[0][2] * matriz[1][1] * matriz[2][0]) - (matriz[0][0] * matriz[1][2] * matriz[2][1]) - (matriz[0][1] * matriz[1][0] * matriz[2][2]))
-        print(f"la solucion es {solucion}")
+        print("La solución es:")
+        print(matriz[0][0] * matriz[1][1] * matriz[2][2] + matriz[0][1] * matriz[1][2] * matriz[2][0] + matriz[0][2] * matriz[1][0] * matriz[2][1] - matriz[0][2] * matriz[1][1] * matriz[2][0] - matriz[0][1] * matriz[1][0] * matriz[2][2] - matriz[0][0] * matriz[1][2] * matriz[2][1])
     else:
         print("La matriz no es cuadrada de 3 x 3.")
 
 
-sarrus([[1, 20, 3], [4, 50, 6], [7, 80, 9]])
+sarrus([[1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]])
+
